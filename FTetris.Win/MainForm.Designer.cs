94a1552a196c049fa2d7c15f180b867aa6ec8d8b
Fujio Kojima <fujiwo@hotmail.co.jp>
2016-02-14 18:49:19 +0900

    FTetris.Win 追加

---
+++ FTetris.Win/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FTetris.Win
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(631, 754);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.Url = new System.Uri("http://www2.shos.info/FTetris.TS/index.html", System.UriKind.Absolute);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 754);
+            this.Controls.Add(this.webBrowser);
+            this.Name = "MainForm";
+            this.Text = "FTetris";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
--- FTetris.WinForm/MainForm.Designer.cs
+++ FTetris.WinForm/MainForm.Designer.cs
@@ -37,12 +37,14 @@
             // statusBar
             // 
             this.statusBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusBar.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.nextPolyominoStatusLabel,
             this.scoreStatusLabel});
             this.statusBar.Location = new System.Drawing.Point(0, 0);
             this.statusBar.Name = "statusBar";
-            this.statusBar.Size = new System.Drawing.Size(264, 22);
+            this.statusBar.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusBar.Size = new System.Drawing.Size(248, 22);
             this.statusBar.SizingGrip = false;
             this.statusBar.TabIndex = 1;
             // 
@@ -63,18 +65,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.gameBoardView.BackColor = System.Drawing.Color.Black;
             this.gameBoardView.DataContext = null;
-            this.gameBoardView.Location = new System.Drawing.Point(0, 25);
+            this.gameBoardView.Location = new System.Drawing.Point(0, 31);
+            this.gameBoardView.Margin = new System.Windows.Forms.Padding(5);
             this.gameBoardView.Name = "gameBoardView";
-            this.gameBoardView.Size = new System.Drawing.Size(264, 656);
+            this.gameBoardView.Size = new System.Drawing.Size(248, 647);
             this.gameBoardView.TabIndex = 0;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 681);
+            this.ClientSize = new System.Drawing.Size(248, 678);
             this.Controls.Add(this.statusBar);
             this.Controls.Add(this.gameBoardView);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "FTetris(Enter: Start ←: Left →: Right ↑: Turn Right ↓: Turn Left Space: Drop)";
             this.statusBar.ResumeLayout(false);
